fe83bf6519be924641776928b5c2ed4805c5c8e2
Javier-Cabana <fcojcz@gmail.com>
2025-04-26 13:22:58 +0200

    Puesta en marcha

---
+++ src/main/java/com/fcojcz/FocusListAPI/FocusListApiApplication.java
@@ -0,0 +1,13 @@
+package com.fcojcz.FocusListAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcojcz/FocusListAPI/FocusListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcojcz.FocusListAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
