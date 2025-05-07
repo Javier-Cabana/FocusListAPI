c3a5154aa782cbf262bc848e1ff57e236ce3eb4e
Javier-Cabana <fcojcz@gmail.com>
2025-05-07 20:59:34 +0200

    Configuraciion de seguridad terminada

--- src/main/java/com/fcojcz/FocusListAPI/security/SecurityConfig.java
+++ src/main/java/com/fcojcz/FocusListAPI/security/SecurityConfig.java
@@ -51,7 +51,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // 4. SecurityFilterChain con la nueva DSL de lambdas
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
