6d36416950a8eb78b6106c44462190d9ca5888fe
Javier-Cabana <fcojcz@gmail.com>
2025-05-10 16:15:35 +0200

    DTOs de Lista y Tarea creados

--- src/main/java/com/fcojcz/FocusListAPI/model/dto/tarea/TareaDeleteDTO.java
+++ src/main/java/com/fcojcz/FocusListAPI/model/dto/tarea/TareaDeleteDTO.java
@@ -1,4 +1,16 @@
 package com.fcojcz.FocusListAPI.model.dto.tarea;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TareaDeleteDTO {
+
+    @NotNull(message = "El id de la tarea es obligatorio")
+    private UUID id;
 }
