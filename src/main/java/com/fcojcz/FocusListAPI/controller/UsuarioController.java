0309853cdacea13c0addfece75b3f0c7f0538d8f
Javier-Cabana <fcojcz@gmail.com>
2025-05-10 12:14:16 +0200

    Swagger funcionando

--- src/main/java/com/fcojcz/FocusListAPI/controller/UsuarioController.java
+++ src/main/java/com/fcojcz/FocusListAPI/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UsuarioController {
     @Autowired
