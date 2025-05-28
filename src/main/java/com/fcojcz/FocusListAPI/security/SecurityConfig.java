97d47a06f53dd37451864912aab680b0bd8fa3ee
Javier-Cabana <fcojcz@gmail.com>
2025-05-28 21:47:08 +0200

    Cambios para arregalr CORS

--- src/main/java/com/fcojcz/FocusListAPI/security/SecurityConfig.java
+++ src/main/java/com/fcojcz/FocusListAPI/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(withDefaults())
+            .cors(Customizer.withDefaults())
 
             .csrf(AbstractHttpConfigurer::disable)
 
